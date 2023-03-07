01858075aadf69a7c57ccb5afd03e31b95e7aa06
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 08:18:57 -0800

    Test: initialize test

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,3 +1,13 @@
 require 'rspec'
 require './lib/item'
 require './lib/vendor'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  it 'exists' do
+    expect(@vendor).to be_a(Vendor)
+  end
+end
