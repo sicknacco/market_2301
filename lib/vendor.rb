a5fe2efbf274c9029c6d8b69d9befa3075b65ad2
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 11:58:21 -0800

    Feat: START OVER

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -5,23 +5,14 @@ class Vendor
 
   def initialize(name)
     @name = name
-    @inventory = {}
-    @stock = 0
+    @inventory = Hash.new(0)
   end
 
   def check_stock(item)
-    @stock
+    @inventory[item]
   end
 
-  def stock(item, count)
-    if @inventory.count == 0
-      @inventory[item] = count
-    elsif
-      @inventory[item] += count
-    else
-      # @inventory = {}
-      # @inventory[item] = count
-    end
-    @stock += count
+  def stock(item, amount)
+    @inventory[item] += amount
   end
 end
