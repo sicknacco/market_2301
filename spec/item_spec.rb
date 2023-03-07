9cd79a7dbebdf2d9648ff9921500e5dc1ba3bfcc
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 08:17:08 -0800

    Test: reveal attribute tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe Item do
   it 'exists and has attributes' do
     expect(@item1).to be_a(Item)
     expect(@item2).to be_a(Item)
-  #   expect(@item1.name).to eq("Peach")
-  #   expect(@item2.name).to eq("Tomato")
-  #   expect(@item1.price).to eq(.75)
-  #   expect(@item2.price).to eq(.5)
-  # end
+    expect(@item1.name).to eq("Peach")
+    expect(@item2.name).to eq("Tomato")
+    expect(@item1.price).to eq(0.75)
+    expect(@item2.price).to eq(0.5)
+  end
 end
