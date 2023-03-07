b4b7867407c7ccc058d6696e02ee4a7e6bcb926a
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 12:58:44 -0800

    Test: add vendors test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -6,6 +6,11 @@ require './lib/market'
 RSpec.describe Market do
   before(:each) do 
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   it 'exists and has a name' do
@@ -15,5 +20,10 @@ RSpec.describe Market do
 
   it 'starts with no vendors' do
     expect(@market.vendors).to eq([])
+  end
+
+  it 'can add vendors' do
+    @market.add_vendor(@vendor1)
+    expect(@market.vendors).to eq([@vendor1])
   end
 end
