18bd44a4a11afadc4c917b2837fff9cfeb2af512
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 13:24:55 -0800

    Test: added more vendors to stock tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Market do
   before(:each) do 
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
@@ -26,4 +27,22 @@ RSpec.describe Market do
     @market.add_vendor(@vendor1)
     expect(@market.vendors).to eq([@vendor1])
   end
+
+  it 'vendor can stock items' do
+    @market.add_vendor(@vendor1)
+    @vendor1.stock(@item1, 35)
+    expect(@vendor1.check_stock(@item1)).to eq(35)
+
+    @vendor1.stock(@item2, 7)
+    expect(@vendor1.check_stock(@item2)).to eq(7)
+    expect(@vendor1.inventory).to eq({@item1 => 35, @item2 => 7})
+    
+    @market.add_vendor(@vendor2)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    expect(@vendor2.inventory).to eq({@item4 => 50, @item3 => 25})
+    
+  end
+
+
 end
