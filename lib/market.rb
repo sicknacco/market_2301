3d929a445fe52f31ae86a231c3541900a2c3b3d1
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 13:43:27 -0800

    Feat: vendor names method

--- lib/market.rb
+++ lib/market.rb
@@ -10,4 +10,10 @@ class Market
   def add_vendor(vendor)
     @vendors << vendor
   end
+
+  def vendor_names #map returns a new array with just the info we want
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
 end
