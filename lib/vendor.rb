add2152699230c8ad41d15d048b400ae458733b8
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 12:06:22 -0800

    Feat: Initialize Vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,5 @@
 class Vendor
-  
+  def initialize(name)
+    
+  end
 end  
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,3 +2,12 @@ require 'rspec'
 require './lib/item'
 require './lib/vendor'
 
+RSpec.describe Vendor do
+  before(:each) do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  it 'exists and has a name' do
+    expect(@vendor).to be_a(Vendor)
+  end
+end
