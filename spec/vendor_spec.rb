2423c76f7d98311ab21427bd287501f77f613b9d
Nick Sacco <nicksacco10@gmail.com>
2023-03-07 09:58:44 -0800

    Trying to figure out check_stock again

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,8 @@ require './lib/vendor'
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   it 'exists and has a name' do
@@ -18,7 +20,10 @@ RSpec.describe Vendor do
   end
 
   it 'can stock items' do
-    @vendor.stock(item1, 30)
-    expect(@vendor.inventory).to eq({:item1 => 30})
+    @vendor.stock(@item1, 30)
+    expect(@vendor.inventory).to eq({@item1 => 30})
+    expect(@vendor.check_stock(@item1)).to eq(30)
+    # @vendor.stock(@item1, 30)
+    # expect(@vendor.inventory).to eq({@item1 => 60})
   end
 end
