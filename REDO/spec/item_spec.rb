5afcd94d5c845d0e5c866fcfa821035d42122a16
Nick Sacco <nicksacco10@gmail.com>
2023-03-08 07:06:23 -0800

    Initialize Item class

--- REDO/spec/item_spec.rb
+++ REDO/spec/item_spec.rb
@@ -4,7 +4,16 @@ require './lib/item'
 RSpec.describe Item do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
-  it 
+  it 'exists and has attributes' do
+    expect(@item1).to be_a(Item)
+    expect(@item1.name).to eq('Peach')
+    expect(@item1.price).to eq(0.75)
+
+    expect(@item2).to be_a(Item)
+    expect(@item2.name).to eq('Tomato')
+    expect(@item2.price).to eq(0.5)
+  end
 end
